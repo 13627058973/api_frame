dadd96716999e2a91e99e6cc52be0de756bbf9b0
aizhiying <1350248796@qq.com>
2021-04-01 10:11:43 +0800

    添加aa代码文件

---
+++ prictice/aa.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# encoding: utf-8
+"""
+@Time : 2021-04-01 上午 10:10
+@project : api_frame
+@Author  : xhb
+@Site    : 
+@File    : aa.py
+@Software: PyCharm Community Edition
+"""
+
+print("aa")
